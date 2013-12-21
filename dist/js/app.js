97dad43bb6185087aebdad02726a11c8f2ade3be
jlaswell <jlaswell@purdue.edu>
2013-12-20 22:59:41 -0500

    Adding sidebar and fake tasks.

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,10 +1,22 @@
-angular.module('todo', ['ionic'])
+angular.module('todo', ['ionic', 'ngRoute', 'ngAnimate'])
 
-.controller('TodoCtrl', function($scope) {
+.controller('TodoCtrl', function($scope, $http) {
   $scope.tasks = [
-    { title: 'Collect coins' },
-    { title: 'Eat mushrooms' },
-    { title: 'Get high enough to grab the flag' },
-    { title: 'Find the Princess' }
+    { complete: true, title: 'Collect coins' },
+    { complete: true, title: 'Eat mushrooms' },
+    { complete: false, title: 'Get high enough to grab the flag' },
+    { complete: false, title: 'Find the Princess' }
   ];
+
+  $scope.toggleMenu = function() {
+	  $scope.sideMenuController.toggleLeft();
+	};
+
+  $http.get('/')
+  	.success(function(tasks) {
+  		$scope.loaded = true;
+  		$scope.tasks = tasks;
+  	}).error(function(err) {
+  		alert(err);
+  	});
 });
